6f3d3039e581cb3f1b148351fb5d6055dfefd3b6
Alex Sam <alsam3637@gmail.com>
2023-11-15 09:49:55 -0500

    Edit Category.

--- app/Http/Controllers/admin/CategoryController.php
+++ app/Http/Controllers/admin/CategoryController.php
@@ -12,7 +12,8 @@ use Image;
 
 class CategoryController extends Controller
 {
-    public function index(Request $request){
+    public function index(Request $request)
+    {
         $categories = Category::latest();
 
         if (!empty($request->get('keyword')))
@@ -25,11 +26,13 @@ class CategoryController extends Controller
         return view('admin.categories.list', compact('categories'));
     }
 
-    public function create(){
+    public function create()
+    {
         return view('admin.categories.create');
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         $validator = Validator::make($request->all(), [
             'name' =>'required',
             'slug' =>'required|unique:categories',
@@ -57,7 +60,10 @@ class CategoryController extends Controller
                 // Generate Image Thumbnail
                 $dPath = public_path() . '/uploads/category/thumb/' . $newImageName;
                 $img = Image::make($sPath);
-                $img->resize(450, 600);
+                //$img->resize(450, 600);
+                $img->fit(450, 600, function ($constraint) {
+                    $constraint->upsize();
+                });
                 $img->save($dPath);
 
                 $category->image = $newImageName;
@@ -79,7 +85,81 @@ class CategoryController extends Controller
         }
     }
 
-    public function edit(){}
-    public function update(){}
+    public function edit(Request $request, int $categoryId)
+    {
+        $category = Category::find($categoryId);
+        if (empty($category)) {
+            return redirect()->route('categories.index');
+        }
+
+        return view('admin.categories.edit', compact('category'));
+    }
+    public function update(Request $request, int $categoryId )
+    {
+        $category = Category::find($categoryId);
+        if (empty($category)) {
+            return response()->json([
+                'status' => false,
+                'notFound' => true,
+                'message' => 'Category not found',
+            ]);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name' =>'required',
+            'slug' =>'required|unique:categories,slug,' . $category->id . ',id',
+        ]);
+
+        if ($validator->passes()) {
+
+            $category->name = $request->name;
+            $category->slug = $request->slug;
+            $category->status = $request->status;
+            $category->save();
+
+            $oldImage = $category->image;
+
+            // Save image
+            if (!empty($request->image_id)) {
+                $tempImage = TempImage::find($request->image_id);
+                $extArray = explode('.', $tempImage->name);
+                $ext = last($extArray);
+
+                $newImageName = $category->id . '-' . time() . '.' . $ext;
+                $sPath = public_path() . '/temp/' . $tempImage->name;
+                $dPath = public_path() . '/uploads/category/' . $newImageName;
+                File::copy($sPath, $dPath);
+
+                // Generate Image Thumbnail
+                $dPath = public_path() . '/uploads/category/thumb/' . $newImageName;
+                $img = Image::make($sPath);
+                //$img->resize(450, 600);
+                $img->fit(450, 600, function ($constraint) {
+                    $constraint->upsize();
+                });
+                $img->save($dPath);
+
+                $category->image = $newImageName;
+                $category->save();
+
+                // Remove Old Images.
+                File::delete(public_path() . '/uploads/category/thumb/' . $oldImage);
+                File::delete(public_path() . '/uploads/category/' . $oldImage);
+            }
+
+            $request->session()->flash('success', 'Category updated successfully');
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Category updated successfully',
+            ]);
+
+        } else {
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors(),
+            ]);
+        }
+    }
     public function destroy(){}
 }
